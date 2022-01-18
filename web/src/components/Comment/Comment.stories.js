2a5377b698dd25dc3eab7deeecf9310440eef248
EasierByCode[43] <daniel@community43.org>
2022-01-18 15:32:03 -0700

    style Comment story

--- web/src/components/Comment/Comment.stories.js
+++ web/src/components/Comment/Comment.stories.js
@@ -2,13 +2,15 @@ import Comment from './Comment'
 
 export const generated = () => {
   return (
-    <Comment
-      comment={{
-        name: 'Rob Cameron',
-        body: 'This is the first comment!',
-        createdAt: '2020-01-01T12:34:56Z',
-      }}
-    />
+    <div className="m-4">
+      <Comment
+        comment={{
+          name: 'Rob Cameron',
+          body: 'This is the first comment!',
+          createdAt: '2020-01-01T12:34:56Z',
+        }}
+      />
+    </div>
   )
 }
 
