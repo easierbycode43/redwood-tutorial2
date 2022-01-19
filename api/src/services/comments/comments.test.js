ee717b4009b68031b23c7b50de37e9256b441add
EasierByCode[43] <daniel@community43.org>
2022-01-19 15:45:51 -0700

    Add Comments to the Schema

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,9 +1,25 @@
-import { comments } from './comments'
+import { comments, createComment } from './comments'
 
 describe('comments', () => {
   scenario('returns all comments', async (scenario) => {
     const result = await comments()
 
     expect(result.length).toEqual(Object.keys(scenario.comment).length)
+  })
+
+  // optional first argument ('postOnly') is the named scenario to use
+  scenario('postOnly', 'creates a new comment', async (scenario) => {
+    const comment = await createComment({
+      input: {
+        name: 'Billy Bob',
+        body: 'What is your favorite tree bark?',
+        postId: scenario.post.bark.id,
+      },
+    })
+
+    expect(comment.name).toEqual('Billy Bob')
+    expect(comment.body).toEqual('What is your favorite tree bark?')
+    expect(comment.postId).toEqual(scenario.post.bark.id)
+    expect(comment.createdAt).not.toEqual(null)
   })
 })
